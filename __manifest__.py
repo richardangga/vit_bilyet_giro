4d92137b3a85f4fdd8f93b38bf025dddd2b54efa
richard.angga <richard.angga51@gmail.com>
2020-01-03 11:26:40 +0700

    commit bilyet

---
+++ __manifest__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vit_bilyet_giro",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """\
+
+        Features
+        ======================================================================
+
+        * mencatat data cek dan giro yg dikeluarkan atau diterima perusahaan untuk membayar hutang atau pelunasan piutang
+        * created menu:
+            * Accounting / Giro / Giro
+        * created object
+            * vit.giro
+        * created views
+            * giro
+            * invoice
+        * logic:
+            * user mencatat giro dan mengalokasikan ke invoice-invoice yg hendak di bayar
+            * user bisa lihat daftar giro yg jatuh tempo per hari
+            * jika dicek ke rek bank, giro tersebut sdh clearing maka user klik tombol clearing
+            * system akan membuat invoice payment sesuai alokasi pada giro
+
+
+        Special thanks to Mr Tiongsin for the business logics :)
+
+    """,
+
+    'author': "akhmad.daniel@gmail.com,richardangga51@gmail.com",
+    'category': "Accounting",
+    'website': "http://www.vitraining.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account','account_voucher'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/invoice.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}   
---
+++ models/models.py
@@ -0,0 +1,109 @@
+from odoo import models, fields, api, _
+import time
+import logging
+import odoo.addons.decimal_precision as dp
+import datetime
+
+STATES = [('draft', 'Draft'), ('open', 'Open'), ('close', 'Close'), ('reject', 'Reject')]
+
+class vit_bilyet_giro(models.Model):
+    _name = "vit.vit_bilyet_giro"
+
+    name = fields.Char(string="Number", help="Nomor Giro", readonly=True, states={'draft':[('readonly', False)]})
+    due_date = fields.Date(string="Due Date", readonly=True, states={'draft':[('readonly', False)]})
+    receive_date = fields.Date(string="Receive Date", readonly=True, states={'draft':[('readonly', False)]})
+    clearing_date = fields.Datetime(string="Clearing Date", readonly=True, states={'draft':[('readonly', False)]})
+    amount = fields.Float(string="Amount", readonly=True, states={'draft':[('readonly', False)]})
+    partner_id = fields.Many2one(comodel_name="res.partner", string="Partner", readonly=True, states={'draft':[('readonly', False)]})
+    journal_id = fields.Many2one(comodel_name="account.journal", string="Bank Journal", domain=[('type', '=', 'bank')], readonly=True, states={'draft':[('readonly', False)]})
+    giro_invoice_ids = fields.One2many(comodel_name="vit.giro_invoice", inverse_name="giro_id", readonly=True, states={'draft':[('readonly', False)]})
+    invoice_names = fields.Char(string="Allocated Invoices", compute='_get_invoices')
+    type = fields.Selection([('payment','Payment'),('receipt','Receipt')], string="Type", readonly=True, required=True, states={'draft':[('readonly', False)]})
+    invoice_type = fields.Char(string="Invoice Type", readonly=True, states={'draft':[('readonly', False)]})
+    state = fields.Selection(string="State", selection=STATES, required=True, readonly=True, default=STATES[0][0])
+    # _sql_constraints = [('name_uniq', 'unique(name)', _('Nomor Giro tidak boleh sama'))]
+    
+    @api.depends
+    def _get_invoices(self):
+        results = {}
+        for giro in self.env():
+            results[giro.id] = ""
+            for gi in giro.giro_invoice_ids:
+                results[giro.id] += "%s " % (gi.invoice_id.number or "")
+        return results
+    
+    @api.multi
+    def _cek_total(self):
+        inv_total = 0.0
+        for giro in self.env():
+            for gi in giro.giro_invoice_ids:
+                inv_total += gi.amount
+            if giro.amount == inv_total:
+                return True
+        return False
+    _constraints = [(_cek_total, _('Total amount allocated for the invoices must be the same as total Giro amount'), ['amount', 'giro_invoice_ids'])]
+    _defaults = {
+        'state': STATES[0][0],
+        'receive_date': time.strftime("%Y-%m-%d %H:%M:%S"),
+        'type': 'payment',
+        'inv_type': 'in_invoice',
+    }
+    
+    @api.multi
+    def action_cancel(self):
+        self.write({'state': STATES[0][0]})
+        
+    @api.multi
+    def action_confirm(self):
+        self.write({'state': STATES[1][0]})
+        
+    @api.multi
+    def action_clearing(self):     
+        voucher_obj = self.env['account.voucher']
+        users_obj = self.env['res.users']
+        company_id = users_obj.company_id.id
+        # print(users_obj.company_id.name)
+        # print("===================")
+        
+        for giro in self:
+            for gi in giro.giro_invoice_ids:
+                invoice_id = gi.invoice_id
+                partner_id = giro.partner_id.id
+                amount = gi.amount
+                journal_id = giro.journal_id
+                type = giro.type
+                name = giro.name
+                vid = voucher_obj.create_payment()
+                # voucher_obj.payment_confirm()        
+        self.write({'state': STATES[2][0], 'clearing_date': time.strftime("%Y-%m-%d %H:%M:%S")})
+    
+    @api.multi
+    def action_reject(self):
+        self.write({'state': STATES[3][0]})
+    
+    @api.onchange('type')
+    def on_change_type(self):
+        inv_type = 'in_invoice'
+        if self.type == 'payment':
+            inv_type = 'in_invoice'
+        elif self.type == 'receipt':
+            inv_type = 'out_invoice'
+        self.invoice_type = inv_type
+
+class vit_giro_invoice(models.Model):
+    _name = "vit.giro_invoice"
+    
+    giro_id = fields.Many2one(comodel_name="vit.vit_bilyet_giro", string="Giro")
+    invoice_id = fields.Many2one(comodel_name="account.invoice", string="Invoice", help="Invoice to be paid", domain=[('state', '=', 'open')])
+    amount_invoice = fields.Float(string="Invoice Amount")
+    amount = fields.Float(string="Amount Allocated")
+    
+    @api.onchange('invoice_id')
+    def on_change_invoice_id(self):
+        self.amount_invoice = self.invoice_id.residual
+        
+class account_invoice(models.Model):
+    _name = "account.invoice"
+    _inherit = 'account.invoice'
+    
+    giro_invoice_ids = fields.One2many(comodel_name="vit.giro_invoice", inverse_name="invoice_id", string="Giro")
